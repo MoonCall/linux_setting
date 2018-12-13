fe3a5a0cf6bb0b30931c34936199cddbfdd72d3b
MoonCall <moonchang@postech.ac.kr>
2018-12-14 01:58:38 +0900

    add ln's strange behavior handling

--- create-symlink.sh
+++ create-symlink.sh
@@ -3,10 +3,16 @@
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 ln -s -f $SCRIPT_PATH/bash/bashrc ~/.bashrc
-ln -s -f $SCRIPT_PATH/vim/ ~/.vim
+ln -s -f $SCRIPT_PATH/vim ~/.vim
 ln -s -f $SCRIPT_PATH/vim/vimrc ~/.vimrc
-ln -s -f $SCRIPT_PATH/script/ ~/.script
-ln -s -f $SCRIPT_PATH/tools/ ~/.tools
+ln -s -f $SCRIPT_PATH/script ~/.script
+ln -s -f $SCRIPT_PATH/tools ~/.tools
 ln -s -f $SCRIPT_PATH/gdb/gdbinit ~/.gdbinit
 ln -s -f $SCRIPT_PATH/vim/pathogen/autoload $SCRIPT_PATH/vim/autoload
+
+# ln's strange action
+rm $SCRIPT_PATH/vim/vim
+rm $SCRIPT_PATH/script/script
+rm $SCRIPT_PATH/tools/tools
+rm $SCRIPT_PATH/vim/pathogen/autoload/autoload
 echo "DONE CREATE_SYMLINK"
