5ea484542a7317456f91bae364e2a01de0aa14a7
MoonCall <mooncall@ip-172-31-15-23.ap-northeast-2.compute.internal>
2018-12-13 17:01:22 +0000

    refine create-symlink.sh

--- create-symlink.sh
+++ create-symlink.sh
@@ -2,12 +2,10 @@
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-ln -s -f $SCRIPT_PATH/bash/bashrc ~/.bashrc
+# directory link
 ln -s -f $SCRIPT_PATH/vim ~/.vim
-ln -s -f $SCRIPT_PATH/vim/vimrc ~/.vimrc
 ln -s -f $SCRIPT_PATH/script ~/.script
 ln -s -f $SCRIPT_PATH/tools ~/.tools
-ln -s -f $SCRIPT_PATH/gdb/gdbinit ~/.gdbinit
 ln -s -f $SCRIPT_PATH/vim/pathogen/autoload $SCRIPT_PATH/vim/autoload
 
 # ln's strange action
@@ -15,4 +13,10 @@ rm $SCRIPT_PATH/vim/vim
 rm $SCRIPT_PATH/script/script
 rm $SCRIPT_PATH/tools/tools
 rm $SCRIPT_PATH/vim/pathogen/autoload/autoload
+
+# file link
+ln -s -f $SCRIPT_PATH/bash/bashrc ~/.bashrc
+ln -s -f $SCRIPT_PATH/vim/vimrc ~/.vimrc
+ln -s -f $SCRIPT_PATH/gdb/gdbinit ~/.gdbinit
+
 echo "DONE CREATE_SYMLINK"
