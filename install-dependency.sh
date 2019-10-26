e26f2262360958bd7f6a844aa28e2b2ab139157a
MoonCall <moonchang@postech.ac.kr>
2019-10-26 11:37:05 +0000

    add pwndbg

--- install-dependency.sh
+++ install-dependency.sh
@@ -1,10 +1,18 @@
 #! /bin/bash
 set -e
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
 UNAME=$(uname)
 if [ "$UNAME" = "Darwin" ]; then
   echo "Install dependency for Mac"
   brew install CMake
 fi
 if [ "$UNAME" = "Linux" ]; then
+  sudo apt-get update
+  sudo apt-get install -y git curl rlwrap
+  sudo apt-get install -y build-essential cmake python3-dev
+  sudo apt-get install -y gdb
+  $SCRIPT_PATH/tools/pwndbg/setup.sh
   echo "Install dependency for Linux"
 fi
--- install-global-package.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-sudo apt-get update
-sudo apt-get install -y git curl rlwrap
-echo "DONE INSTALL_GLOBAL_PACKAGE"
