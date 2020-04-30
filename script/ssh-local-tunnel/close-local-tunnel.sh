b8e430eb1d79f4981c508af79796f99e75f89648
MoonCha <moonchang@postech.ac.kr>
2020-04-30 08:58:10 +0000

    update tunneling scripts to get ssh port as a param

--- script/ssh-local-tunnel/close-local-tunnel.sh
+++ script/ssh-local-tunnel/close-local-tunnel.sh
@@ -8,8 +8,9 @@ pushd $scriptdir > /dev/null
     port=${parsed_array[0]}
     dest_addr=${parsed_array[1]}
     nat_addr=${parsed_array[2]}
+    ssh_port=${parsed_array[3]}
 
-    echo "CLOSE tunnel on localhost:$port to $dest_addr"
+    echo "CLOSE tunnel on localhost:$port to $dest_addr through $nat_addr:$ssh_port"
 
     ssh -S _socket_$port -O exit $nat_addr
 
--- script/ssh-local-tunnel/open-local-tunnel.sh
+++ script/ssh-local-tunnel/open-local-tunnel.sh
@@ -8,10 +8,11 @@ pushd $scriptdir > /dev/null
     port=${parsed_array[0]}
     dest_addr=${parsed_array[1]}
     nat_addr=${parsed_array[2]}
+    ssh_port=${parsed_array[3]}
 
-    echo "OPEN tunnel on localhost:$port to $dest_addr"
+    echo "OPEN tunnel on localhost:$port to $dest_addr through $nat_addr:$ssh_port"
 
-    ssh -L $port:$dest_addr $nat_addr -fnNT -M -S _socket_$port
+    ssh -p$ssh_port -L $port:$dest_addr $nat_addr -fnNT -M -S _socket_$port
 
   done < "tunnels.txt"
 }
