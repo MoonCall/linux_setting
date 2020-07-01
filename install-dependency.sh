8344fb9020c95ff53e82e49b05dbcfc89a64625f
USER <user@AL01484742.local>
2020-07-01 14:54:22 +0900

    add brew and coreutils as default dependency for Mac

--- install-dependency.sh
+++ install-dependency.sh
@@ -6,9 +6,12 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 UNAME=$(uname)
 if [ "$UNAME" = "Darwin" ]; then
   echo "Install dependency for Mac"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   brew install CMake
+  brew install coreutils
 fi
 if [ "$UNAME" = "Linux" ]; then
+  echo "Install dependency for Linux"
   sudo apt-get update
   sudo apt-get install -y git curl rlwrap
   sudo apt-get install -y build-essential cmake python3-dev
@@ -16,5 +19,4 @@ if [ "$UNAME" = "Linux" ]; then
   pushd $SCRIPT_PATH/tools/pwndbg
   $SCRIPT_PATH/tools/pwndbg/setup.sh
   popd
-  echo "Install dependency for Linux"
 fi
